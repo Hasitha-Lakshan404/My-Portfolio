8a02dcf5d15b89b2c9c6fd2ae0af3e650d8e74c2
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-09-23 15:59:32 +0530

    Try to Create Theme using enable Disable

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -4,8 +4,40 @@ function dark() {
 
     if (a.checked === true) {
         b.style.backgroundColor = "#242734";
+        // $("#AboutMe-section").removeAttr("style");
+
+
+        /*for the disable*/
+        $("#style1").attr("disabled", "disabled");
+
     } else {
+        /*light THeme*/
+
+        /*$("<link/>", {
+            rel: "stylesheet",
+            type: "text/css",
+            href: "../styleSheet/lightTheme/lightAbout.css"
+        }).appendTo("body");*/
+
+
+
+        // $("#AboutMe-section").removeAttr("style");
+
+
+
+
         // b.style.backgroundColor = "white";
         b.style.backgroundColor = '#320A48';
+
+
+        // $("#style1").attr("disabled", "disabled");
+        $("#style1").removeAttr("disabled");
+
+
+       /* $(document).ready(function () {
+            $('#AboutMe-section').css({"border":"5px solid black"});
+            $('<style type="text/css"></style>')
+                .html('@import url("../../styleSheet/lightTheme/lightAbout.css")');
+        });*/
     }
 }
