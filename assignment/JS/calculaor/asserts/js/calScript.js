8b2b84fc74391dab688185125176616b42a163b0
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-09-25 18:25:53 +0530

    Catch All No using JQuery

--- assignment/JS/calculaor/asserts/js/calScript.js
+++ assignment/JS/calculaor/asserts/js/calScript.js
@@ -1,3 +1,40 @@
+
+$("#calNo0").click(function () {
+    console.log("0")
+})
+
 $("#calNo1").click(function () {
-    alert("1")
+    console.log("1")
+})
+
+$("#calNo2").click(function () {
+    console.log("2")
+})
+
+$("#calNo3").click(function () {
+    console.log("3")
+})
+
+$("#calNo4").click(function () {
+    console.log("4")
+})
+
+$("#calNo5").click(function () {
+    console.log("5")
+})
+
+$("#calNo6").click(function () {
+    console.log("6")
+})
+
+$("#calNo7").click(function () {
+    console.log("7")
+})
+
+$("#calNo8").click(function () {
+    console.log("8")
+})
+
+$("#calNo9").click(function () {
+    console.log("9")
 })
