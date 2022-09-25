01c8a5f27b4613a8e431ce95543a211446334cb9
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-09-25 18:41:06 +0530

    Display Current Number

--- assignment/JS/calculaor/asserts/js/calScript.js
+++ assignment/JS/calculaor/asserts/js/calScript.js
@@ -3,59 +3,71 @@
 
 /*========== NO 0 ===========*/
 $("#calNo0").click(function () {
-    console.log("0")
+    console.log("0");
+    $("#calCurrentNumber").text("0");
 })
 
 
 /*========== NO 1 ===========*/
 $("#calNo1").click(function () {
-    console.log("1")
+    console.log("1");
+    $("#calCurrentNumber").text("1");
 })
 
 
 /*========== NO 2 ===========*/
 $("#calNo2").click(function () {
-    console.log("2")
+    console.log("2");
+    $("#calCurrentNumber").text("2");
 })
 
 
 /*========== NO 3 ===========*/
 $("#calNo3").click(function () {
-    console.log("3")
+    console.log("3");
+    $("#calCurrentNumber").text("3");
 })
 
 
 /*========== NO 4 ===========*/
 $("#calNo4").click(function () {
-    console.log("4")
+    console.log("4");
+    $("#calCurrentNumber").text("4");
 })
 
 
 /*========== NO 5 ===========*/
 $("#calNo5").click(function () {
-    console.log("5")
+    console.log("5");
+    $("#calCurrentNumber").text("5");
 })
 
 
 /*========== NO 6 ===========*/
 $("#calNo6").click(function () {
-    console.log("6")
+    console.log("6");
+    $("#calCurrentNumber").text("6");
 })
 
 
 /*========== NO 7 ===========*/
 $("#calNo7").click(function () {
-    console.log("7")
+    console.log("7");
+    $("#calCurrentNumber").text("7");
 })
 
 
 /*========== NO 8 ===========*/
 $("#calNo8").click(function () {
-    console.log("8")
+    console.log("8");
+    $("#calCurrentNumber").text("8");
 })
 
 
 /*========== NO 9 ===========*/
 $("#calNo9").click(function () {
-    console.log("9")
+    console.log("9");
+    $("#calCurrentNumber").text("9");
+
 })
+
