a9d8d82f371e77efd5c02069afee71897edc6ef1
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-09-25 18:27:51 +0530

    Content Cleaned

--- assignment/JS/calculaor/asserts/js/calScript.js
+++ assignment/JS/calculaor/asserts/js/calScript.js
@@ -1,40 +1,61 @@
 
+
+
+/*========== NO 0 ===========*/
 $("#calNo0").click(function () {
     console.log("0")
 })
 
+
+/*========== NO 1 ===========*/
 $("#calNo1").click(function () {
     console.log("1")
 })
 
+
+/*========== NO 2 ===========*/
 $("#calNo2").click(function () {
     console.log("2")
 })
 
+
+/*========== NO 3 ===========*/
 $("#calNo3").click(function () {
     console.log("3")
 })
 
+
+/*========== NO 4 ===========*/
 $("#calNo4").click(function () {
     console.log("4")
 })
 
+
+/*========== NO 5 ===========*/
 $("#calNo5").click(function () {
     console.log("5")
 })
 
+
+/*========== NO 6 ===========*/
 $("#calNo6").click(function () {
     console.log("6")
 })
 
+
+/*========== NO 7 ===========*/
 $("#calNo7").click(function () {
     console.log("7")
 })
 
+
+/*========== NO 8 ===========*/
 $("#calNo8").click(function () {
     console.log("8")
 })
 
+
+/*========== NO 9 ===========*/
 $("#calNo9").click(function () {
     console.log("9")
 })
