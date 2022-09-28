73d5f6b9ffb11c2d08eeee55b2dec9406ab197fe
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-09-28 21:44:44 +0530

    Implemented Button event and cus Object

---
+++ assignment/CSS/Frameworks/asserts/js/customerScript.js
@@ -0,0 +1,13 @@
+$('#btnSaveCustomer').click(function () {
+
+
+    //cus Object
+    var cusObject={
+        cusId:$('#customerId').val(),
+        cusName:$('#customerName').val(),
+        cusAddress:$('#customerAddress').val(),
+        cusSalary:$('#customerSalary').val()
+    }
+
+
+})
