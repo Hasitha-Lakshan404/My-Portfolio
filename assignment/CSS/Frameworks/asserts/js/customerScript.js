5536dbe7cd57c3e830f0ae475d52b48dc7ad6d1a
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-09-29 14:40:42 +0530

    Customer Table Completed

--- assignment/CSS/Frameworks/asserts/js/customerScript.js
+++ assignment/CSS/Frameworks/asserts/js/customerScript.js
@@ -2,17 +2,43 @@ var customer=[];
 
 $('#btnSaveCustomer').click(function () {
 
-    alert("nsa")
-    console.log("sdasdasa");
+    let customerID = $("#customerId").val();
+    let customerName = $("#customerName").val();
+    let customerAddress = $("#customerAddress").val();
+    let customerSalary = $("#customerSalary").val();
+
     //cus Object
     var cusObject={
-        cusId:$('#customerId').val(),
-        cusName:$('#customerName').val(),
-        cusAddress:$('#customerAddress').val(),
-        cusSalary:$('#customerSalary').val()
+        cusId:customerID,
+        cusName:customerName,
+        cusAddress:customerAddress,
+        cusSalary:customerSalary
     }
+
     customer.push(cusObject);
-    console.log(customer);
+    setData();
+
+
+    /*// get all customer records from the array
+    for(var customersL of customer){
+        console.log(customer);// customer object
+
+        // add those data to the table row
+        // var row= "<tr><td>"+customer.id+"</td><td>"+customer.name+"</td><td>"+customer.address+"</td><td>"+customer.salary+"</td></tr>";
+
+        // Using String Literals to do the same thing as above
+        var row= `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.salary}</td></tr>`;
+
+        //then add it to the table body of customer table
+        $("#tblCustomer").append(row);
+    }*/
 });
 
+function setData() {
+    for (let customerElement of customer) {
+        var row=`<tr><td>${customerElement.cusId}</td><td>${customerElement.cusName}</td><td>${customerElement.cusAddress}</td><td>${customerElement.cusSalary}</td></tr>`;
+        // var rows=`<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.salary}</td></tr>`;
 
+        $('#tblCustomer').append(row);
+    }
+}
