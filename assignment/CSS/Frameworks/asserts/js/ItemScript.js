b12b4e209005b8aa2c13952e81625fe945e4be9f
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-09-29 15:45:32 +0530

    Implemented Item Save

--- assignment/CSS/Frameworks/asserts/js/ItemScript.js
+++ assignment/CSS/Frameworks/asserts/js/ItemScript.js
@@ -1,4 +1,22 @@
+var item=[];
+
 
 $('#itemSaveButton').click(function () {
+    
+    let itemCode=$("#item-code");
+    let itemName =$('#item-name');
+    let itemPrice =$('#item-Price');
+    let itemQty =$('#item-qty');
+    
+    var itemObject= {
+        itCode:itemCode,
+        itName:itemPrice,
+        itPrice:itemPrice,
+        itQty:itemQty
+    }
 
-})
+    item.push(itemObject);
+
+    setItemData();
+});
+
