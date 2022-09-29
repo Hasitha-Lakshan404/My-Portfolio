6fd132e7e50b7399436492795ec0fb0bc71abd69
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-09-29 15:46:06 +0530

    Implemented method to the Set Item Data to  the tab;e

--- assignment/CSS/Frameworks/asserts/js/ItemScript.js
+++ assignment/CSS/Frameworks/asserts/js/ItemScript.js
@@ -20,3 +20,13 @@ $('#itemSaveButton').click(function () {
     setItemData();
 });
 
+function setItemData() {
+    $('#tblItem').empty();
+
+    for (let itemElement of item) {
+        var row=`<tr><td>${itemElement.itCode}</td><td>${itemElement.itName}</td><td>${itemElement.itPrice}</td><td>${itemElement.itQty}</td></tr>`;
+
+        $('#tblItem').append(row);
+    }
+}
+
