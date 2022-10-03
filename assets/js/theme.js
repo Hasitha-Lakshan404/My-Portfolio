ba1d248fb748743ad8ed14cf9b2f1001e902bb6e
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-10-03 16:33:05 +0530

    Added j-query for the mouse

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -49,8 +49,6 @@ function dark() {
         // $("#dQStyle").attr("disabled","disabled");
         // $("#dImgStyle").attr("disabled","disabled");
 
-
-
         b.style.backgroundColor = "#E7EBF8";
         // b.style.backgroundColor = '#320A48';
 
@@ -58,3 +56,17 @@ function dark() {
 
     }
 }
+
+
+//for the Cursor
+$(document).mousemove(function(event) {
+    // currentMousePos.x = event.pageX;
+    // currentMousePos.y = event.pageY;
+    console.log(event.pageY+"----"+event.pageX);
+
+    $('#mainCircle').css({
+        'top':event.pageY-20,
+        'left':event.pageX-20
+    });
+
+});
