b1c312b3ea844ea6e4828d098a3320f7127dbba4
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-10-07 02:06:10 +0530

    added New Load Style Color

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -70,3 +70,20 @@ $(document).mousemove(function(event) {
     });
 
 });
+
+
+//for the Loading Window
+$(window).on('load',function () {
+    // $('#loadingContainer').fadeOut(1000);
+
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+        (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/6311aa7f54f06e12d8924ec1/1gbug40vk';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+
+})
