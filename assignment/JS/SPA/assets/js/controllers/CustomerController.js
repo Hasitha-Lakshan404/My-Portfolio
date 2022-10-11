f572efb10a6d42af11868f1fc0c38d54fb50dbd9
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-10-11 21:37:32 +0530

    Implement Regex for the Customer fields

--- assignment/JS/SPA/assets/js/controllers/CustomerController.js
+++ assignment/JS/SPA/assets/js/controllers/CustomerController.js
@@ -214,3 +214,81 @@ function updateCustomer(customerID) {
         return false;
     }
 }
+
+
+
+/*FOr Validation*/
+
+
+$("#txtCustomerID").focus();
+
+// customer reguler expressions
+const cusIDRegEx = /^(C00-)[0-9]{1,3}$/;
+const cusNameRegEx = /^[A-z ]{5,20}$/;
+const cusAddressRegEx = /^[0-9/A-z. ,]{7,}$/;
+const cusSalaryRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+
+let customerValidations = [];
+customerValidations.push({reg: cusIDRegEx, field: $('#customerId'),error:'Customer ID Pattern is Wrong : C00-001'});
+customerValidations.push({reg: cusNameRegEx, field: $('#customerName'),error:'Customer Name Pattern is Wrong : A-z 5-20'});
+customerValidations.push({reg: cusAddressRegEx, field: $('#customerAddress'),error:'Customer Address Pattern is Wrong : A-z 0-9 ,/'});
+customerValidations.push({reg: cusSalaryRegEx, field: $('#customerSalary'),error:'Customer Salary Pattern is Wrong : 100 or 100.00'});
+
+
+
+$("#customerId,#customerName,#customerAddress,#customerSalary").on('keyup', function (event) {
+    checkValidity();
+});
+
+$("#customerId,#customerName,#customerAddress,#customerSalary").on('blur', function (event) {
+    checkValidity();
+});
+
+function checkValidity() {
+    let errorCount=0;
+    for (let validation of customerValidations) {
+        if (check(validation.reg,validation.field)) {
+            textSuccess(validation.field,"");
+        } else {
+            errorCount=errorCount+1;
+            setTextError(validation.field,validation.error);
+        }
+    }
+    setButtonState(errorCount);
+}
+
+function check(regex, txtField) {
+    let inputValue = txtField.val();
+    return regex.test(inputValue) ? true : false;
+}
+
+function textSuccess(txtField,error) {
+    if (txtField.val().length <= 0) {
+        defaultText(txtField,"");
+    } else {
+        txtField.css('border', '2px solid green');
+        txtField.parent().children('span').text(error);
+    }
+}
+
+function setTextError(txtField,error) {
+    if (txtField.val().length <= 0) {
+        defaultText(txtField,"");
+    } else {
+        txtField.css('border', '2px solid red');
+        txtField.parent().children('span').text(error);
+    }
+}
+
+function defaultText(txtField,error) {
+    txtField.css("border", "1px solid #ced4da");
+    txtField.parent().children('span').text(error);
+}
+
+function setButtonState(value){
+    if (value>0){
+        $("#btnSaveCustomer").attr('disabled',true);
+    }else{
+        $("#btnSaveCustomer").attr('disabled',false);
+    }
+}
